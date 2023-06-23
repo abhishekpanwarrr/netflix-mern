f38ce55aaf642e76b675e5ab0ccde635a2130c7d
Abhishek Panwar <abhishekpanwar1718@gmail.com>
2023-06-23 21:00:16 +0530

    completed users routes

--- api/verifyToken.js
+++ api/verifyToken.js
@@ -3,13 +3,15 @@ const jwt = require("jsonwebtoken")
 
 function verify(req, res, next) {
     const authHeader = req.headers.token
-
+    
     if(authHeader) {
         const token = authHeader.split(" ")[1]
-        console.log("🚀 ~ file: verifyToken.js:9 ~ verify ~ token:", token)
 
         jwt.verify(token,process.env.SECRET_KEY,(err,user) =>{
-            if(err) return res.status(304).json({message:"Invalid token"})
+            if(err) {
+                console.log("Error: " + err);
+                return res.status(304).json({message:"Invalid token"})
+            }
             req.user = user
             next()
         })
